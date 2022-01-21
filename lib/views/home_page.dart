2fce9af222a77a612017e2c0735647589908a842
Paulo Santos <afonso.mea@gmail.com>
2022-01-20 21:25:49 -0300

    Adiciona Widget ListWheelScrollView

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -20,19 +20,105 @@ class _MyHomePageState extends State<HomePage> {
             ),
         ),
       ),
-      body:
-        Swiper(
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index){
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image(
-                image: AssetImage(images1[index]),
-                  fit: BoxFit.cover,
-              ),
-            );
-          },
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Swiper(
+              itemCount: 2,
+              viewportFraction: 0.8,
+              scale: 0.9,
+              layout: SwiperLayout.DEFAULT,
+              control: SwiperControl(),
+              pagination: SwiperPagination(),
+              itemBuilder: (BuildContext context, int index){
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image(
+                    image: AssetImage(images1[index]),
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: ListWheelScrollView(
+              itemExtent: 150,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Teste Scroll 1",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 45,
+                      ),
+                    ),
+                  ),
+                ),Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Teste Scroll 2",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 45,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Teste Scroll 3",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 45,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Teste Scroll 4",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 45,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
